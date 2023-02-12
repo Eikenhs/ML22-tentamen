a1fa99ebdf00d316af36b9424837389eed544588
Eikenhs <eikenhorst@gmail.com>
2023-02-12 13:47:59 +0000

    2 extra runs gedraaid

--- dev/scripts/01_model_GRU_design.py
+++ dev/scripts/01_model_GRU_design.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     configs = [
         GRUmodelConfig(
             input=13, output=20, tunedir=presets.logdir,
-            hidden_size=128, num_layers=1, dropout=0.2
+            hidden_size=256, num_layers=4, dropout=0.2
         ),
     ]
 
